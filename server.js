56bcb91da89fbc5d12a8ea7d775c080a5f03e89e
namal tharindu <NamalA701997@gmail.com>
2024-05-11 12:02:45 +0530

    set host

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const productRouter = require('./routes/product.route');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const productRouter = require("./routes/product.route");
 
 const app = express();
 app.use(bodyParser.json());
 
 //Connect to MongoDB
 mongoose.connect(
-  'mongodb+srv://arunapriyankara99ap:aruna99@cluster0.oedlfw9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+	"mongodb+srv://arunapriyankara99ap:aruna99@cluster0.oedlfw9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 );
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
 //Routes
-app.use('/products', productRouter);
+app.use("/products", productRouter);
 
 //Start server
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+const HOST = "0.0.0.0"; // Listen on all available network interfaces
+app.listen(PORT, HOST, () => {
+	console.log(`Server started on http://${HOST}:${PORT}`);
+});
